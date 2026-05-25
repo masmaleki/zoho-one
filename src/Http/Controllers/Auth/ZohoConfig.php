cf16cbe1c359dc2fa2e2b11c95e4bca03e6ff381
Ali Mehraei <ali.mehraei@gmail.com>
2026-05-25 13:11:01 +0330

    feat: ERP-2605 | Implement Zoho Books integration and enhance organization feature checks

--- src/Http/Controllers/Auth/ZohoConfig.php
+++ src/Http/Controllers/Auth/ZohoConfig.php
@@ -8,27 +8,21 @@ class ZohoConfig
 
     public static function getAuthUrl($organizationId = null)
     {
-        $z_api_url = config('zoho-one.api_base_url');
-        $z_current_user_email = config('zoho-one.current_user_email');
         $z_return_url = config('zoho-one.redirect_uri');
         $z_url = config('zoho-one.accounts_url');
 
         if ($organizationId == null) {
             $client_id = config('zoho-one.client_id');
-            $secret_key = config('zoho-one.client_secret');
             $z_oauth_scope = config('zoho-one.oauth_scope');
         } else {
-            $client_id = config('zoho-one.client_id_' . $organizationId);
-            $secret_key = config('zoho-one.client_secret_' . $organizationId);
+            $orgId = (int) $organizationId;
+            $client_id = ZohoOrgCredentials::get($orgId, 'client_id');
+            $z_oauth_scope = ZohoOrgCredentials::get($orgId, 'oauth_scope');
+            $z_url = ZohoOrgCredentials::get($orgId, 'accounts_url') ?: $z_url;
             $z_return_url = "$z_return_url/$organizationId";
-            $z_oauth_scope = config('zoho-one.oauth_scope_' . $organizationId);
         }
 
-        //info('$z_return_url---');
-        //info($z_return_url);
-
         return "$z_url/oauth/v2/auth?scope=$z_oauth_scope&client_id=$client_id&response_type=code&access_type=offline&redirect_uri=$z_return_url";
-
     }
 
 }
--- src/Http/Controllers/Auth/ZohoTokenCheck.php
+++ src/Http/Controllers/Auth/ZohoTokenCheck.php
@@ -37,8 +37,6 @@ class ZohoTokenCheck
     {
         $data = $request->all();
 
-        $z_api_url = config('zoho-one.api_base_url');
-        $z_current_user_email = config('zoho-one.current_user_email');
         $z_return_url = config('zoho-one.redirect_uri');
         $z_url = config('zoho-one.accounts_url');
 
@@ -47,10 +45,11 @@ class ZohoTokenCheck
             $secret_key = config('zoho-one.client_secret');
             $z_oauth_scope = config('zoho-one.oauth_scope');
         } else {
-            $client_id = config('zoho-one.client_id_' . $organizationId);
-            $secret_key = config('zoho-one.client_secret_' . $organizationId);
+            $orgId = (int) $organizationId;
+            $client_id = ZohoOrgCredentials::get($orgId, 'client_id');
+            $secret_key = ZohoOrgCredentials::get($orgId, 'client_secret');
             $z_return_url = "$z_return_url/$organizationId";
-            $z_oauth_scope = config('zoho-one.oauth_scope_' . $organizationId);
+            $z_oauth_scope = ZohoOrgCredentials::get($orgId, 'oauth_scope');
         }
 
         $postInput = [
